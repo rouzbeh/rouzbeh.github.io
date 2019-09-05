1579fa14cb7db726bb1a964acd12115c3168ff32
Ali Neishabouri <ali@spike.ai>
2019-09-05 17:53:03 +0200

    remove old files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sed 's/BASE_PATH :\ false/BASE_PATH :\ \/\~man210/g' _config.yml >_config_deploy.yml
-mv _config.yml _config.yml.bak
-mv _config_deploy.yml _config.yml
-jekyll build
-rsync -avz --delete _site/ shell:/homes/man210/public_html
-ssh shell 'cd ~/public_html && ln -s french French && ln -s mathmatics Mathematics && ln -s tips Tips'
-mv _config.yml.bak _config.yml
-
--- deploy4all.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-jekyll build
-rsync -avz --delete _site/ web4all:/datas/vol3/w4a155529/var/www/neishabouri.net/htdocs
-
